f72535538841fb76c3efdb567f5d5fc3e3e82acf
MRcraigchick <craigchick@outlook.com>
2022-12-07 02:29:37 +0100

    Update details route

--- routes/items.js
+++ routes/items.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { validateReqBody, isObject, firstToUpperCase } from '../lib/helpers.js';
+import { validateReqBody, isObject, firstToUpperCase, isNull } from '../lib/helpers.js';
 import Items from '../db/models/Items.js';
 import Users from '../db/models/Users.js';
 const router = express.Router();
@@ -136,6 +136,61 @@ router.put('/updatestatus', async (req, res) => {
       result: true,
     });
   }
+});
+
+router.put('/updatedetails', async (req, res) => {
+  if (
+    !validateReqBody({
+      body: req.body,
+      expectedPropertys: ['token', 'id', 'details'],
+    })
+  )
+    res.json({ result: false, error: 'Invalid token or data' });
+  else {
+    const { token, id, details } = req.body;
+    const user = await Users.findOne({ token });
+
+    if (!user) {
+      res.json({ result: false, error: 'Invalid token' });
+      return;
+    }
+
+    if (!isObject(details)) {
+      res.json({ result: false, error: 'details must be an object with propertys' });
+      return;
+    }
+
+    if (Object.values(details).some((val) => isNull(val))) {
+      res.json({ result: false, error: 'No null values in details' });
+      return;
+    }
+
+    const item = await Items.findById(id);
+
+    if (!item) {
+      res.json({
+        result: false,
+        error: 'Item not found',
+      });
+      return;
+    }
+
+    if (String(item.userId) !== String(user._id)) {
+      res.json({ result: false, error: 'User unauthorized to edit item' });
+      return;
+    }
+
+    try {
+      item.details = details;
+      item.save();
+    } catch {
+      res.json({ result: false, error: 'Failed to update items details field' });
+      return;
+    }
+  }
+  res.json({
+    result: true,
+  });
 });
 
 export default router;
